5afc1cf55d0804c26de0030e01f97b4ae5ee28b4
Max Gonzih <gonzih@gmail.com>
2017-08-04 10:11:41 +0200

    Add ability to search by title/username/url

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ type responseEntry struct {
 	UserName string `json:"username"`
 	Title    string `json:"title"`
 	Password string `json:"password"`
+	URL      string `json:"url"`
 }
 
 type errorResponse struct {
@@ -31,15 +32,19 @@ func GetUserName(entry *gokeepasslib.Entry) string {
 	return entry.Get("UserName").Value.Content
 }
 
-func findInGroupByUserName(group *gokeepasslib.Group, userName string) (*gokeepasslib.Entry, error) {
+func GetURL(entry *gokeepasslib.Entry) string {
+	return entry.Get("URL").Value.Content
+}
+
+func findInGroupByValue(group *gokeepasslib.Group, key, value string) (*gokeepasslib.Entry, error) {
 	for _, entry := range group.Entries {
-		if userName == GetUserName(&entry) {
+		if value == entry.Get(key).Value.Content {
 			return &entry, nil
 		}
 	}
 
 	for _, innerGroup := range group.Groups {
-		entry, err := findInGroupByUserName(&innerGroup, userName)
+		entry, err := findInGroupByValue(&innerGroup, key, value)
 
 		if err == nil {
 			return entry, err
@@ -53,7 +58,8 @@ func marshalEntry(entry *gokeepasslib.Entry) ([]byte, error) {
 	response := responseEntry{
 		UserName: GetUserName(entry),
 		Title:    entry.GetTitle(),
-		Password: entry.GetPassword()}
+		Password: entry.GetPassword(),
+		URL:      GetURL(entry)}
 
 	return json.Marshal(&response)
 }
@@ -113,21 +119,35 @@ func respondWithError(w http.ResponseWriter, err error, status int) {
 	w.Write(json)
 }
 
-func findEntryByUsername(username string) (*gokeepasslib.Entry, error) {
+func findEntry(key, value string) (*gokeepasslib.Entry, error) {
 	sharedGroupLock.RLock()
 	defer sharedGroupLock.RUnlock()
-	return findInGroupByUserName(&sharedGroup, username)
+	return findInGroupByValue(&sharedGroup, key, value)
 }
 
 func SearchHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var searchKey, searchValue string
 	username := r.FormValue("username")
+	title := r.FormValue("title")
+	url := r.FormValue("url")
 
-	if len(username) == 0 {
-		respondWithError(w, errors.New("username parameter is required"), http.StatusBadRequest)
+	if len(username) == 0 && len(title) == 0 && len(url) == 0 {
+		respondWithError(w, errors.New("from username/title/url at least one parameter is required"), http.StatusBadRequest)
 		return
 	}
 
-	entry, err := findEntryByUsername(username)
+	if len(username) > 0 {
+		searchKey = "UserName"
+		searchValue = username
+	} else if len(title) > 0 {
+		searchKey = "Title"
+		searchValue = title
+	} else if len(url) > 0 {
+		searchKey = "URL"
+		searchValue = url
+	}
+
+	entry, err := findEntry(searchKey, searchValue)
 
 	if err != nil {
 		respondWithError(w, err, http.StatusNotFound)
